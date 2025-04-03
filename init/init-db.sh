676574cb5df67548cd3940242898e0ee3cee06c3
abhinavasr <abhinava.sr@gmail.com>
2025-04-03 09:38:44 -0400

    Update initialization script to execute user attribute table SQL file

--- init/init-db.sh
+++ init/init-db.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 set -e
-
 # Wait for PostgreSQL to be ready
 until PGPASSWORD=guacamole_password psql -h postgres -U guacamole_user -d guacamole_db -c '\q'; do
   >&2 echo "PostgreSQL is unavailable - sleeping"
   sleep 1
 done
-
 >&2 echo "PostgreSQL is up - executing schema scripts"
-
 # Execute schema scripts
 PGPASSWORD=guacamole_password psql -h postgres -U guacamole_user -d guacamole_db -f /schema/01-schema.sql
 PGPASSWORD=guacamole_password psql -h postgres -U guacamole_user -d guacamole_db -f /schema/02-create-admin-user.sql
-
+PGPASSWORD=guacamole_password psql -h postgres -U guacamole_user -d guacamole_db -f /schema/03-user-attribute.sql
 >&2 echo "Schema initialization completed successfully"
